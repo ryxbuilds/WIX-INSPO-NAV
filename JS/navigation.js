c76ad94cfc5fd8ae087d2ee126084c8c275764ca
Patrick <triciodev@gmail.com>
2026-08-06 15:38:54 +0100

    JS commit

--- JS/navigation.js
+++ JS/navigation.js
@@ -2,15 +2,38 @@
 
 const dropDownIcon = document.querySelector(".icon-down");
 const dropUpIcon = document.querySelector(".icon-up");
-const dropContainer = document.querySelector(".nav");
+const dropContainer = document.querySelector(".nav-link");
 const dropDown = document.querySelector(".dropdown");
+
+const showNav = document.querySelector(".main-icon");
+const closeNav = document.querySelector(".icon-switch");
+const mainNav = document.querySelector(".nav-dropdown");
 
 // console.log(dropDown);
 dropContainer.addEventListener("click", function (e) {
   console.log("clicked");
 
+  e.preventDefault();
+
   dropDownIcon.classList.toggle("hidden");
   // if the icon is clicked
   dropUpIcon.classList.toggle("hidden");
   dropDown.classList.toggle("hidden");
+});
+
+showNav.addEventListener("click", function (e) {
+  console.log("show nav is running");
+  e.preventDefault();
+
+  mainNav.classList.toggle("hidden");
+  showNav.classList.toggle("hidden");
+  closeNav.classList.toggle("hidden");
+});
+
+closeNav.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  mainNav.classList.toggle("hidden");
+  showNav.classList.toggle("hidden");
+  closeNav.classList.toggle("hidden");
 });
