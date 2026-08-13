cd1739b45ecdaa35bad8be3ddce97eec0b51f119
Patrick <triciodev@gmail.com>
2026-08-13 16:05:14 +0100

    JS added

--- JS/navigation.js
+++ JS/navigation.js
@@ -1,39 +1,51 @@
 "use strict";
 
-const dropDownIcon = document.querySelector(".icon-down");
-const dropUpIcon = document.querySelector(".icon-up");
-const dropContainer = document.querySelector(".nav-link");
-const dropDown = document.querySelector(".dropdown");
+const nav = document.querySelectorAll(".nav");
 
 const showNav = document.querySelector(".main-icon");
 const closeNav = document.querySelector(".icon-switch");
 const mainNav = document.querySelector(".nav-dropdown");
 
+// const navFunction = function (nav) {
+//   console.log(nav.querySelector(".nav-link"));
+// };
+
+// navFunction(nav);
+
 // console.log(dropDown);
-dropContainer.addEventListener("click", function (e) {
-  console.log("clicked");
+// For the drop down when the something in the navigation is clicked
+const navDropDown = function (nav) {
+  const dropDownIcon = nav.querySelector(".icon-down");
+  const dropUpIcon = nav.querySelector(".icon-up");
+  const dropContainer = nav.querySelector(".nav-link");
+  const dropDown = nav.querySelector(".dropdown");
 
-  e.preventDefault();
+  console.log(nav);
 
-  dropDownIcon.classList.toggle("hidden");
-  // if the icon is clicked
-  dropUpIcon.classList.toggle("hidden");
-  dropDown.classList.toggle("hidden");
-});
+  // dropContainer.addEventListener("click", function (e) {
+  //   console.log("clicked");
 
-showNav.addEventListener("click", function (e) {
+  //   e.preventDefault();
+
+  //   dropDownIcon.classList.toggle("hidden");
+  //   // if the icon is clicked
+  //   dropUpIcon.classList.toggle("hidden");
+  //   dropDown.classList.toggle("hidden");
+  // });
+};
+nav.forEach(navDropDown);
+
+// For what gets shown when the when the menu icon gets clicked
+
+const NavMenuAppearance = function (e) {
   console.log("show nav is running");
+
   e.preventDefault();
 
   mainNav.classList.toggle("hidden");
   showNav.classList.toggle("hidden");
   closeNav.classList.toggle("hidden");
-});
+};
 
-closeNav.addEventListener("click", function (e) {
-  e.preventDefault();
-
-  mainNav.classList.toggle("hidden");
-  showNav.classList.toggle("hidden");
-  closeNav.classList.toggle("hidden");
-});
+closeNav.addEventListener("click", NavMenuAppearance);
+showNav.addEventListener("click", NavMenuAppearance);
